09f915e0416da3a64e2c15ccceabec71bd8ac539
fitzscott <bushnelf@gmail.com>
2017-04-06 23:19:38 -0600

    fix security chief title

--- SecurityChief.py
+++ SecurityChief.py
@@ -17,7 +17,7 @@ class SecurityChief(C.Card):
     """
 
     def __init__(self):
-        C.Card.__init__(self, "SecurityChief", 7)
+        C.Card.__init__(self, "Security Chief", 7)
         self.add_symbol(C.Card.Symbols[2])      # People
         self.add_symbol(C.Card.Symbols[0])      # Negation
 
@@ -28,11 +28,10 @@ class SecurityChief(C.Card):
             if C.Card.Symbols[2] in card.symbols:
                 mypb.victorypoints += 2
                 break        # but only once
-            
+
     def defense(self, game, pbidx, effect=["main_effect"], thisorlast="this"):
         """ prevents VP theft for this round and next round """
         return("vp_theft" in effect)
 
 if __name__ == '__main__':
     d = SecurityChief()
-
