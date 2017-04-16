2097f06ef4c45d740d3649c87609569fd199b75d
fitzscott <bushnelf@gmail.com>
2017-04-16 15:47:37 -0600

    more tests

--- countcombos.py
+++ countcombos.py
@@ -1,9 +1,11 @@
 import math as M
+import CardSet as CS
 
+cl = CS.CardSet(5)
 for i in range(8):
     num_cards = i+1
-    li = 24+i
+    totcards = len(cl.cards)
+    li = totcards - num_cards + i
     combos = M.factorial(li) / (M.factorial(num_cards) *
-                    M.factorial(li - num_cards))
-    print(str(num_cards) + " taken from " + str(li) + " is " + str(combos))
-
+                                M.factorial(li - num_cards))
+    print(str(num_cards) + " taken out of " + str(li) + " is " + str(combos))
